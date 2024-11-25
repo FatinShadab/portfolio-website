3501217f768f436f8446e1ace87ad98b34cfe38f
Naeem Abdullah Sadik <67340415+naeemsadik@users.noreply.github.com>
2024-11-25 23:45:34 +0600

    eibar sure final

--- Frontend/script.js
+++ Frontend/script.js
@@ -46,3 +46,21 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('a[href^="#"]');
+  
+    for (const link of links) {
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+  
+        if (targetElement) {
+          targetElement.scrollIntoView({
+            behavior: 'smooth'
+          });
+        }
+      });
+    }
+  });
